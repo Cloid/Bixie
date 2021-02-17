0f4da1b060aa11b79056ec9ecd1a37c61aa6a03f
Sam Feng <yfeng68@ucsc.edu>
2021-02-17 15:21:40 -0800

    Update Player.cs
    Added a new method called PlayFootsteps

--- Assets/Scroller/Scripts/Player.cs
+++ Assets/Scroller/Scripts/Player.cs
@@ -182,12 +182,16 @@ public class Player : MonoBehaviour {
 
 	public void PlaySound(string path, string parameterName, int parameterValue)
 	{
-		//FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
 		FMOD.Studio.EventInstance sfx = FMODUnity.RuntimeManager.CreateInstance(path);
 		sfx.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(GetComponent<Transform>().position));
 		sfx.setParameterByName(parameterName, parameterValue);
 		sfx.start();
 		sfx.release();
+	}
+
+	public void PlayFootstepsSound(string path)
+	{
+		FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
 	}
 
 	private void OnTriggerStay(Collider other)

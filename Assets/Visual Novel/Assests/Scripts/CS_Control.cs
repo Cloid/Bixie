588256cceab8721050c43bfd003a1160522d4aca
Cloid <nintendogamer@live.com>
2021-03-03 10:30:34 -0800

    Added assests + basic scripts

---
+++ Assets/Visual Novel/Assests/Scripts/CS_Control.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Control : MonoBehaviour
+{
+    public GameObject ml_sel;
+    public GameObject ml_unsel;
+    public GameObject q_sel;
+    public GameObject q_unsel;
+    public GameObject blur;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

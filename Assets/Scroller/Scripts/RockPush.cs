dd522e63277bb682c5a267b86eadad8321341a76
Sheng Yu <syu126@ucsc.edu>
2021-04-24 12:00:21 -0700

    Updated boss cage

--- Assets/Scroller/Scripts/RockPush.cs
+++ Assets/Scroller/Scripts/RockPush.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class RockPush : MonoBehaviour
 {
+    private Rigidbody Boss1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Boss1 = GameObject.Find("Shanxiao_ACT1Boss").GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -15,4 +17,30 @@ public class RockPush : MonoBehaviour
     {
         
     }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "Boss1")
+        {
+            // Shanxiao_BossACT1 moveScript = other.GetComponent<Shanxiao_BossACT1>();
+            Boss1.constraints = RigidbodyConstraints.FreezeAll;
+            attente();
+            // move.enabled=false;
+            // touche = true;
+            // Debug.Log ("Il y a eu collision");
+ 
+        }
+ 
+    }
+
+    IEnumerator attente ()
+    {
+        Boss1.constraints = RigidbodyConstraints.FreezeRotation;
+        yield return new WaitForSeconds (3);
+    }
+
+    // IEnumerator returnattente ()
+    // {
+    //     yield return attente;
+    // }
 }

cdf53f76b95ba9bcac3007f1339306d5e75c6581
Sheng Yu <syu126@ucsc.edu>
2021-05-01 17:14:48 -0700

    boss rock push

--- Assets/Scroller/Scripts/RockPush.cs
+++ Assets/Scroller/Scripts/RockPush.cs
@@ -8,13 +8,15 @@ public class RockPush : MonoBehaviour
     private Rigidbody Boss1_body;
     private Shanxiao_BossACT1 Boss1_script;
     private Animator Boss1_anim;
+    private GameObject wave;
 
     // Start is called before the first frame update
     void Start()
     {
-        Boss1_body = GameObject.Find("Shanxiao_ACT1Boss").GetComponent<Rigidbody>();
-        Boss1_script = GameObject.Find("Shanxiao_ACT1Boss").GetComponent<Shanxiao_BossACT1>();
-        Boss1_anim = GameObject.Find("Shanxiao_ACT1Boss").GetComponent<Animator>();
+        // Boss1_body = GameObject.Find("Shanxiao_ACT1Boss").GetComponent<Rigidbody>();
+        // Boss1_script = GameObject.Find("Shanxiao_ACT1Boss").GetComponent<Shanxiao_BossACT1>();
+        // Boss1_anim = GameObject.Find("Shanxiao_ACT1Boss").GetComponent<Animator>();
+        
 
     }
 
@@ -26,9 +28,10 @@ public class RockPush : MonoBehaviour
 
     void OnTriggerEnter (Collider other)
     {
+        float force = .05f;
         if (other.gameObject.tag == "Boss1")
         {
-            StartCoroutine(stun());
+            // StartCoroutine(stun());
             // print("this should be called after wait");
             // Destroy(gameObject);
             // move.enabled=false;
@@ -36,24 +39,55 @@ public class RockPush : MonoBehaviour
             
  
         }
+
+        if (other.gameObject.tag == "MeiWave")
+        {
+            /*
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY | 
+            RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+            // Calculate Angle Between the collision point and the player
+            Vector3 dir = other.contacts[0].point - transform.position;
+            // We then get the opposite (-Vector3) and normalize it
+            dir = -dir.normalized;
+            Debug.Log(dir);
+            // And finally we add force in the direction of dir and multiply it by force. 
+            // This will push back the player
+            GetComponent<Rigidbody>().AddForce(dir*force);
+
+            // Start playing the grinding sound
+            // stone_sound = FMODUnity.RuntimeManager.CreateInstance("event:/Sounds/Stone_Grind");
+            // FMODUnity.RuntimeManager.AttachInstanceToGameObject(stone_sound, GetComponent<Transform>(), GetComponent<Rigidbody>());
+            // stone_sound.start();
+            // stone_sound.release();
+            */
+            wave = GameObject.FindGameObjectWithTag("MeiWave");
+            Debug.Log("rock has collided!");
+            Debug.Log(transform.position);
+            bool facingRight = (wave.transform.position.x < transform.position.x) ? false : true;
+            if(facingRight){
+                transform.position = new Vector3(transform.position.x - 8, transform.position.y, transform.position.z);
+            }else{
+                transform.position = new Vector3(transform.position.x + 8, transform.position.y, transform.position.z);
+            }
+        }
  
     }
 
-    IEnumerator stun()
-    {
-        Debug.Log ("with rock boss");
-        // Shanxiao_BossACT1 moveScript = other.GetComponent<Shanxiao_BossACT1>();
-        Boss1_body.constraints = RigidbodyConstraints.FreezeAll;
-        Boss1_script.enabled = false;
-        Boss1_anim.enabled = false;
-        yield return new WaitForSeconds(stunTime);
+    // IEnumerator stun()
+    // {
+    //     Debug.Log ("with rock boss");
+    //     // Shanxiao_BossACT1 moveScript = other.GetComponent<Shanxiao_BossACT1>();
+    //     Boss1_body.constraints = RigidbodyConstraints.FreezeAll;
+    //     Boss1_script.enabled = false;
+    //     Boss1_anim.enabled = false;
+    //     yield return new WaitForSeconds(stunTime);
         
-        Boss1_body.constraints = RigidbodyConstraints.FreezeRotation;
-        Boss1_script.enabled = true;
-        Boss1_anim.enabled = true;
-        Destroy(gameObject);
+    //     Boss1_body.constraints = RigidbodyConstraints.FreezeRotation;
+    //     Boss1_script.enabled = true;
+    //     Boss1_anim.enabled = true;
+    //     Destroy(gameObject);
         
-    }
+    // }
 
     // IEnumerator returnattente ()
     // {

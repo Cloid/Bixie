b7dc08f4ee85c81b6347a5da6bb97c0f7ccc237b
Cloid <nintendogamer@live.com>
2021-05-03 15:27:55 -0700

    Fixed statue push

--- Assets/Scroller/Scripts/StatueCollider.cs
+++ Assets/Scroller/Scripts/StatueCollider.cs
@@ -38,7 +38,7 @@ public void PushingStatue(float attackDir){
         Debug.Log("test2");
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY | 
         RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
-        Vector3 work = new Vector3(30,0,0);
+        Vector3 work = new Vector3(30*attackDir,0,0);
         GetComponent<Rigidbody>().AddForce(work,ForceMode.Impulse);
         // Start playing the grinding sound
         //stone_sound = FMODUnity.RuntimeManager.CreateInstance("event:/Sounds/Stone_Grind");

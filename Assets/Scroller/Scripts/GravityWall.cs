ce6ef72406a78d0accc7f01ddde4b76b833768b5
Cloid <nintendogamer@live.com>
2021-05-10 15:44:45 -0700

    Fixed statue puzzle

--- Assets/Scroller/Scripts/GravityWall.cs
+++ Assets/Scroller/Scripts/GravityWall.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GravityWall : MonoBehaviour
 {
+    public GameObject Pushey;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +19,13 @@ public class GravityWall : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Statue"){
-            Rigidbody rb = other.GetComponent<Rigidbody>();
-            rb.constraints = RigidbodyConstraints.FreezePositionX | 
+        var statuePos = other.gameObject.GetComponent<Transform>().position;  
+        statuePos.y = Pushey.gameObject.transform.position.y;
+        statuePos.z = Pushey.gameObject.transform.position.z;
+        statuePos.x = Pushey.gameObject.transform.position.x-5;
+        other.gameObject.GetComponent<Transform>().position = statuePos;                    
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezePositionX | 
         RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
             rb.useGravity = true;
         }
